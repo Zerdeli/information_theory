12633aed91365a2d508182b839a21911bff41e68
Aliya Izmailova <59693961+Izmailova-Aliya@users.noreply.github.com>
2021-04-09 15:36:30 +0600

    This a algorithm of finding a probability of each distinct characted from a text file (ass2.txt)

---
+++ ass2InfTheory.py
@@ -0,0 +1,12 @@
+all_symbols = []
+repetition = []
+dict = {}
+with open('ass2.txt', encoding='utf-8') as t:
+    txt = t.read()
+for symbol in txt:
+    count = txt.count(symbol)
+    if count >= 1:
+        dict[symbol] = round(count/len(txt), 4)
+print(txt)
+for key, value in sorted(dict.items()):
+    print(key, ' - ', value)
